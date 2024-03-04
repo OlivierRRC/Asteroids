4d7b4cb09b2592bcb7881967b70076c673272e47
Olivier Proulx <oproulx@academic.rrc.ca>
2024-03-04 10:43:47 -0600

    commented bullet.js

--- js/gameplay/bullet.js
+++ js/gameplay/bullet.js
@@ -1,16 +1,22 @@
 class Bullet extends GameObject {
   constructor(bounds, objects, pos, rot, screenShake) {
-    super(bounds, 8);
+    super(bounds, 10);
+    this.objects = objects;
     this.position = pos;
     this.rotation = rot;
-    this.objects = objects;
 
+    this.speed = 5;
+
+    //time untill the bullet is removed
     this.lifetime = 3;
     this.timer = millis();
+
     this.screenShake = screenShake;
+    //when the object is spawned in, it adds some screenshake
     this.screenShake.add(1);
   }
 
+  //when collided with, add some screenshake and remove from objects array
   collide() {
     this.screenShake.add(3);
     let index = this.objects.indexOf(this);
@@ -20,14 +26,19 @@ class Bullet extends GameObject {
   update() {
     push();
     this.transform();
+
+    //figure out the direction vector and travel in that direction
     let x = 1 * cos(this.rotation - PI / 2);
     let y = 1 * sin(this.rotation - PI / 2);
+    this.position.add(createVector(x, y).mult(this.speed));
 
-    this.position.add(createVector(x, y).mult(3));
+    //draw point at bullets position
     strokeWeight(4);
     point(0, 0);
+
     pop();
 
+    //if enough time has passed, destroy the bullet
     if (millis() - this.timer > this.lifetime * 1000) {
       let index = this.objects.indexOf(this);
       this.objects.splice(index, 1);
