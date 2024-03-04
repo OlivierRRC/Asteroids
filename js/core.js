9c3d150e6a15e47e981042c49ee15f45b1040ac9
Olivier Proulx <oproulx@academic.rrc.ca>
2024-03-04 09:34:18 -0600

    commented core.js

--- js/core.js
+++ js/core.js
@@ -1,10 +1,12 @@
 let font;
 
+//a variable for each of the screens that will be displayed
 let main;
 let leaderBoard;
 let gameOver;
 let game;
 
+//this is basically an enum
 let states = {
   current: 0,
   main: 0,
@@ -13,30 +15,38 @@ let states = {
   gameOver: 3,
 };
 
+//array of scores for the leaderboard
 let scores = [];
 
+//will be assigned a vector that is x = width  and y = height
 let bounds;
 
+//load the font to be used for the text in the game
 function preload() {
   font = loadFont("Assets/NovaMono-Regular.ttf");
 }
 
 function setup() {
+  //uncomment to clear the leaderboard
   //storeItem("scores", null);
+
+  //if there are scores in the local storage, assign the scores variable to them
   if (getItem("scores") != null) {
     scores = getItem("scores");
   }
 
   bounds = createVector(640, 480);
 
+  //create all the different screens
   leaderBoard = new LeaderBoard(transMouse, states, bounds, scores);
   gameOver = new GameOver(transMouse, states, bounds, scores);
   main = new MainMenu(transMouse, states, bounds);
   game = new Game(states, bounds, gameOver);
-
+  //both gameover and game need references to eacheother, so this is how I handle that
   gameOver.setGame(game);
 
   createCanvas(bounds.x, bounds.y);
+  //set the current game state to be showing the main menu
   states.current = states.main;
 }
 
@@ -48,11 +58,14 @@ function draw() {
   textAlign(CENTER, CENTER);
   textFont(font);
 
+  //push pop context to contain the glow effect
   push();
 
+  //these lines create the glow effect
   drawingContext.shadowBlur = 15;
   drawingContext.shadowColor = color(255);
 
+  //look through the enum and show the correct screen based on the value
   switch (states.current) {
     case states.main:
       main.draw();
@@ -69,6 +82,7 @@ function draw() {
   }
   pop();
 
+  //crt grill outside of the push pop so that it dosent have glow
   crtGrill();
 }
 
@@ -84,15 +98,18 @@ function crtGrill() {
   }
 }
 
+//send the keycodes to the gameover screen if it's active
 function keyPressed() {
   if (states.current == states.gameOver) {
     gameOver.processKeyboard(keyCode);
   }
 }
 
-//get mouse wheel delta and save how far the mouse wheel should have scrolled
+//get mouse wheel delta and save how far the mouse wheel should have scrolled on the leaderboard
 function mouseWheel(e) {
-  leaderBoard.scroll(e.delta / 5);
+  if (states.current == states.leaderboard) {
+    leaderBoard.scroll(e.delta / 5);
+  }
 }
 
 //Function that returns mouse coordinates relative to the current transformations
@@ -104,6 +121,5 @@ let transMouse = function () {
     .transformPoint(
       createVector(mouseX * pixelDensity(), mouseY * pixelDensity())
     );
-  //console.log(localCoord);
   return localCoord;
 };
