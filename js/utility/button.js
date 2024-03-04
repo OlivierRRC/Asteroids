37e4038a1b96ca55eef5b6d9295cfb540d06556a
Olivier Proulx <oproulx@academic.rrc.ca>
2024-03-04 09:25:36 -0600

    commented button.js

--- js/utility/button.js
+++ js/utility/button.js
@@ -1,38 +1,42 @@
 class Button {
   constructor(x, y, size, t, f) {
-    this.f = f;
+    this.f = f; //function
     this.x = x;
     this.y = y;
-    this.t = t;
+    this.t = t; //string to be displayed
 
-    this.h = size;
+    this.h = size; //height
     this.lock = false;
   }
 
   draw() {
-    textSize(this.h);
+    textSize(this.h); //sets text size so that the characters are a specific height
     this.w = textWidth(this.t); //for whatever reason I have to do this every frame
 
+    //offset the text by -7 in its height to center it on the bounding box
     text(this.t, this.x, this.y - 7);
 
+    //uncomment to see bounding box
+    /*
     rectMode(CENTER);
     noFill();
-    //rect(this.x, this.y, this.w, this.h);
+    rect(this.x, this.y, this.w, this.h);
+    */
   }
 
   processMouse(mPos) {
     fill(255);
-    drawingContext.shadowColor = color(255);
+    drawingContext.shadowColor = color(255); //change the color of the glow to match the color of the text
     if (this.hovered(mPos)) {
       fill(170);
-      drawingContext.shadowColor = color(170);
+      drawingContext.shadowColor = color(170); //change the color of the glow to match the color of the text
     }
 
     if (mouseIsPressed) {
       if (!this.hovered(mPos)) {
         this.lock = true;
       } else if (!this.lock) {
-        this.f();
+        this.f(); //execute funcion when the button is clicked
         this.lock = true;
       }
     } else {
@@ -44,6 +48,6 @@ class Button {
     let xBounds = this.x + this.w / 2 > mPos.x && this.x - this.w / 2 < mPos.x;
     let yBounds = this.y + this.h / 2 > mPos.y && this.y - this.h / 2 < mPos.y;
 
-    return xBounds && yBounds;
+    return xBounds && yBounds; //return true if the mouse is within the button's bounding box
   }
 }
