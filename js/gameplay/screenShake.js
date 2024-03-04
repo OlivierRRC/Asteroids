403ecd77a752d0590075141205ce32db184aa490
Olivier Proulx <oproulx@academic.rrc.ca>
2024-03-04 10:15:59 -0600

    commented screenshake.js

--- js/gameplay/screenShake.js
+++ js/gameplay/screenShake.js
@@ -5,19 +5,24 @@ class ScreenShake {
   }
 
   update() {
+    //makes the amplitude smaller every frame
     if (this.amplitude > 0) {
       this.amplitude -= 0.1;
     } else {
-      return;
+      return; //if the amplitude is smaller than or = 0 do nothing
     }
 
+    //create a random vector every frame
     this.v.x = random(-1, 1);
     this.v.y = random(-1, 1);
+    //set the magnitude of the random vector to the desired amplitude
     this.v.setMag(this.amplitude);
+    //translate based on the vector
     translate(this.v);
   }
 
   add(n) {
+    //adds to the amplitude
     this.amplitude += n;
   }
 }
