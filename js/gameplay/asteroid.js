65047c6bae1b04080a81c13631965f19d2bbb802
Olivier Proulx <oproulx@academic.rrc.ca>
2024-03-04 11:02:46 -0600

    commented asteroid.js

--- js/gameplay/asteroid.js
+++ js/gameplay/asteroid.js
@@ -2,17 +2,37 @@ class Asteroid extends GameObject {
   constructor(bounds, objects, startPos, startRot, size, generation) {
     super(bounds, size);
 
-    this.generation = generation;
     this.objects = objects;
-    this.resolution = 20;
-    this.noiseScale = 3;
-    this.s = size;
-    this.points = [];
-    this.generateShape();
     this.rotation = startRot;
     this.position = startPos;
+    this.s = size;
+
+    this.generation = generation; //generation starts at 2 and goes down to 0, numbers corespond to asteroid size
+
+    this.resolution = 20; //how many segments the asteroid is drawn out of
+    this.noiseScale = 3;
+
+    this.points = [];
+    this.generateShape();
   }
 
+  //generate a roundish shape as an array of points offset by noise
+  generateShape() {
+    for (let i = 0; i < this.resolution; i++) {
+      let x = 1 * cos((TWO_PI / this.resolution) * i);
+      let y = 1 * sin((TWO_PI / this.resolution) * i);
+      let n = noise(
+        (x + 0.5) * this.noiseScale,
+        (y + 0.5) * this.noiseScale,
+        random(10)
+      );
+      n = map(n, 0, 1, 0.5, 1);
+      this.points.push(createVector(x * this.s * n, y * this.s * n));
+    }
+  }
+
+  //when collided with, create two asteroids of a smaller generation, unless this astroid is of generation 0
+  //remove this asteroid from the objects array
   collide() {
     if (this.generation != 0) {
       for (let i = 0; i < 2; i++) {
@@ -32,6 +52,7 @@ class Asteroid extends GameObject {
     this.objects.splice(index, 1);
   }
 
+  //add some amount to the position vector
   update() {
     push();
     this.transform();
@@ -44,20 +65,7 @@ class Asteroid extends GameObject {
     pop();
   }
 
-  generateShape() {
-    for (let i = 0; i < this.resolution; i++) {
-      let x = 1 * cos((TWO_PI / this.resolution) * i);
-      let y = 1 * sin((TWO_PI / this.resolution) * i);
-      let n = noise(
-        (x + 0.5) * this.noiseScale,
-        (y + 0.5) * this.noiseScale,
-        random(10)
-      );
-      n = map(n, 0, 1, 0.5, 1);
-      this.points.push(createVector(x * this.s * n, y * this.s * n));
-    }
-  }
-
+  //draw the generated shape
   draw() {
     noFill();
     strokeWeight(4);
@@ -66,6 +74,5 @@ class Asteroid extends GameObject {
       vertex(this.points[i].x, this.points[i].y);
     }
     endShape(CLOSE);
-    //circle(0, 0, this.s);
   }
 }
